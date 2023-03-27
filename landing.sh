5dcd0f631419d7aefdee8dc1de1952ae6c3712c5
NAF <66379886+nfelix23@users.noreply.github.com>
2023-03-27 19:09:11 -0300

    Update landing.sh

--- landing.sh
+++ landing.sh
@@ -1,3 +1,3 @@
-wget -P /home/hadoop/landing https://github.com/fpineyro/homework-O/blob/master/starwars.csv
-hdfs dfs -put /home/landing/yellow_tripdata_2021-01.csv /ingest
+wget -P /home/hadoop/landing https://github.com/fpineyro/homework-O/blob/master/starwars.csv \
+hdfs dfs -put /home/landing/yellow_tripdata_2021-01.csv /ingest \
 rm /home/hadoop/landing/starwars.csv
